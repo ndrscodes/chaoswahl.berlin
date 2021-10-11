41a223530cafdbe712b8de6ae22c761eebe4fd33
ndrscodes <98schmidtandreas@gmail.com>
2021-10-11 21:58:32 +0200

    improve tag generation

--- public/home.php
+++ public/home.php
@@ -24,6 +24,12 @@ function get_address_value($name, $default = ""){
     }
     return $default;
 }
+function get_value_tag($value){
+    if(!empty($value)){
+        return 'value="'.$value.'"';
+    }
+    return "";
+}
 ?>
 <head>
     <link rel="stylesheet" href="css/main.css" />
@@ -76,60 +82,60 @@ Fehlende Stimmzettel / zu wenig Wahlkabinen / so lange Wartezeiten, dass Wähler
             <div class="content-inner">
                 <h2>Persönliche Daten</h2>
                 <label for="firstname">Vorname*</label>
-                <input autocomplete="given-name" type="text" id="firstname" name="firstname" required value="<?php echo get_creator_value('firstname') ?>" /><br />
+                <input autocomplete="given-name" type="text" id="firstname" name="firstname" required <?php echo get_value_tag(get_creator_value('firstname')); ?> /><br />
                 <label for="lastname">Nachname*</label>
-                <input autocomplete="family-name" type="text" id="lastname" name="lastname" required value="<?php echo get_creator_value('lastname') ?>" /><br />
+                <input autocomplete="family-name" type="text" id="lastname" name="lastname" required <?php echo get_value_tag(get_creator_value('lastname')); ?> /><br />
                 <label for="address-city">Ort (inkl. PLZ)*</label>
-                <input autocomplete="address-level2" type="text" id="address-city" name="address-city" required value="<?php echo get_address_value('city') ?>" /><br />
+                <input autocomplete="address-level2" type="text" id="address-city" name="address-city" required <?php echo get_value_tag(get_address_value('city')); ?> /><br />
                 <label for="address-street">Straße*</label>
-                <input autocomplete="address-level3" type="text" id="address-street" name="address-street" required value="<?php echo get_address_value('street') ?>" /><br />
+                <input autocomplete="address-level3" type="text" id="address-street" name="address-street" required <?php echo get_value_tag(get_address_value('street')); ?> /><br />
                 <label for="address-number">Hausnummer (inkl. Adresszusatz)*</label>
-                <input autocomplete="address-level4" type="text" id="address-number" name="address-number" required value="<?php echo get_address_value('number') ?>" /><br />
+                <input autocomplete="address-level4" type="text" id="address-number" name="address-number" required <?php echo get_value_tag(get_address_value('number')); ?> /><br />
                 <label for="dateofbirth">Geburtsdatum*</label>
-                <input autocomplete="bday"type="date" name="dateofbirth" id="dateofbirth" value="<?php echo empty(get_creator_value('birth')) ? '' : date_format(get_creator_value('birth'), 'Y-m-d') ?>" required /><br />
+                <input autocomplete="bday"type="date" name="dateofbirth" id="dateofbirth" <?php echo get_value_tag(empty(get_creator_value('birth')) ? '' : date_format(get_creator_value('birth'), 'Y-m-d')); ?>" required /><br />
                 <label for="phone">Telefonnummer</label>
-                <input autocomplete="tel"type="number" name="phone" id="phone" value="<?php echo get_creator_value('phone') ?>" /><br />
+                <input autocomplete="tel"type="number" name="phone" id="phone" <?php echo get_value_tag(get_creator_value('phone')); ?> /><br />
                 <label for="email">E-Mail</label>
-                <input autocomplete="email" type="email" name="email" id="email" value="<?php echo get_creator_value('mail') ?>" /><br />
+                <input autocomplete="email" type="email" name="email" id="email" <?php echo get_value_tag(get_creator_value('mail')); ?> /><br />
             </div>
             <div class="content-inner">
                 <h2>Angaben zum Wahllokal*</h2>
                 <label for="region">Wahlbezirk</label>
-                <input autocomplete="off" type="text" id="region" name="district" required value="<?php echo get_value('district') ?>" /><br />
+                <input autocomplete="off" type="text" id="region" name="district" required <?php echo get_value_tag(get_value('district')); ?> /><br />
                 <label for="location">Wahllokal</label>
-                <input autocomplete="off" type="text" id="location" name="room" required value="<?php echo get_value('room') ?>" /><br />
+                <input autocomplete="off" type="text" id="location" name="room" required <?php echo get_value_tag(get_value('room')); ?> /><br />
             </div>
             <div class="content-inner">
                 <h2>Art der Wahl*</h2>
                 <fieldset>
                     <input type="radio" name="type" id="parliament" value="parliament" checked />
                     <label for="parliament">Bundestag</label><br />
-                    <input type="radio" name="type" id="house-of-representatives" value="house-of-representatives" <?php echo get_value('type') == PollType::HOUSE_OF_REPRESENTATIVES ? 'checked' : '' ?> />
+                    <input type="radio" name="type" id="house-of-representatives" value="house-of-representatives" <?php echo get_value('type') == PollType::HOUSE_OF_REPRESENTATIVES ? 'checked' : ''; ?> />
                     <label for="agh">Abgeordnetenhaus</label><br />
-                    <input type="radio" name="type" id="district-assembly" value="district-assembly" <?php echo get_value('type') == PollType::DISTRICT_ASSEMBLY ? 'checked' : '' ?> />
+                    <input type="radio" name="type" id="district-assembly" value="district-assembly" <?php echo get_value('type') == PollType::DISTRICT_ASSEMBLY ? 'checked' : ''; ?> />
                     <label for="bvv">Bezirksverordnetenversammlung</label><br />
-                    <input type="radio" name="type" id="all" value="all" <?php echo get_value('type') == PollType::ALL ? 'checked' : '' ?> />
+                    <input type="radio" name="type" id="all" value="all" <?php echo get_value('type') == PollType::ALL ? 'checked' : ''; ?> />
                     <label for="all">Alle/mehrere (unten spezifizieren)</label>
                 </fieldset>
             </div>
             <div class="content-inner">
                 <h2>Art des Problems*</h2>
-                <input type="checkbox" id="long-wait-time" name="long-wait-time" <?php echo in_array(ProblemType::LONG_WAIT, get_value('problems', array())) ? 'checked' : '' ?> />
+                <input type="checkbox" id="long-wait-time" name="long-wait-time" <?php echo in_array(ProblemType::LONG_WAIT, get_value('problems', array())) ? 'checked' : ''; ?> />
                 <label for="long-wait-time">Lange Wartezeit</label><br />
-                <input type="checkbox" id="wrong-ballot" name="wrong-ballot" <?php echo in_array(ProblemType::WRONG_BALLOT, get_value('problems', array())) ? 'checked' : '' ?> />
+                <input type="checkbox" id="wrong-ballot" name="wrong-ballot" <?php echo in_array(ProblemType::WRONG_BALLOT, get_value('problems', array())) ? 'checked' : ''; ?> />
                 <label for="wrong-ballot">Falsche Stimmzettel</label><br />
-                <input type="checkbox" id="late-result" name="late-result" <?php echo in_array(ProblemType::LATE_RESULT, get_value('problems', array())) ? 'checked' : '' ?> />
+                <input type="checkbox" id="late-result" name="late-result" <?php echo in_array(ProblemType::LATE_RESULT, get_value('problems', array())) ? 'checked' : ''; ?> />
                 <label for="late-result">Stimmabgabe nach Bekanntgabe der Hochrechnungen</label><br />
-                    <input type="checkbox" id="wrong-result" name="wrong-result" <?php echo in_array(ProblemType::WRONG_RESULT, get_value('problems', array())) ? 'checked' : '' ?> />
+                    <input type="checkbox" id="wrong-result" name="wrong-result" <?php echo in_array(ProblemType::WRONG_RESULT, get_value('problems', array())) ? 'checked' : ''; ?> />
                     <label for="wrong-result">Evidenter Auszählungsfehler (bspw. Stimme nicht gezählt)</label><br />
-                    <input type="checkbox" id="other" name="other" <?php echo in_array(ProblemType::OTHER, get_value('problems', array())) ? 'checked' : '' ?> />
+                    <input type="checkbox" id="other" name="other" <?php echo in_array(ProblemType::OTHER, get_value('problems', array())) ? 'checked' : ''; ?> />
                     <label for="other">Sonstiges</label>
                 </div>
             </div>
             <div class="content-container full-width">
                 <div class="content-inner">
                     <h2>Beschreibung*</h2>
-                    <textarea id="description" name="description" required><?php echo get_value('description') ?></textarea>
+                    <textarea id="description" name="description" required><?php echo get_value('description'); ?></textarea>
                 </div>
             </div>
             <div class="content-container full-width">
