742f4342d981414d1c37db8f96514aa49f95552f
ndrscodes <98schmidtandreas@gmail.com>
2021-10-11 16:21:13 +0200

    label mandatory inputs

--- public/home.php
+++ public/home.php
@@ -75,17 +75,17 @@ Fehlende Stimmzettel / zu wenig Wahlkabinen / so lange Wartezeiten, dass Wähler
         <div class="content-container">
             <div class="content-inner">
                 <h2>Persönliche Daten</h2>
-                <label for="firstname">Vorname</label>
+                <label for="firstname">Vorname*</label>
                 <input autocomplete="given-name" type="text" id="firstname" name="firstname" required value="<?php echo get_creator_value('firstname') ?>" /><br />
-                <label for="lastname">Nachname</label>
+                <label for="lastname">Nachname*</label>
                 <input autocomplete="family-name" type="text" id="lastname" name="lastname" required value="<?php echo get_creator_value('lastname') ?>" /><br />
-                <label for="address-city">Ort (inkl. PLZ)</label>
+                <label for="address-city">Ort (inkl. PLZ)*</label>
                 <input autocomplete="address-level2" type="text" id="address-city" name="address-city" required value="<?php echo get_address_value('city') ?>" /><br />
-                <label for="address-street">Straße</label>
+                <label for="address-street">Straße*</label>
                 <input autocomplete="address-level3" type="text" id="address-street" name="address-street" required value="<?php echo get_address_value('street') ?>" /><br />
-                <label for="address-number">Hausnummer (inkl. Adresszusatz)</label>
+                <label for="address-number">Hausnummer (inkl. Adresszusatz)*</label>
                 <input autocomplete="address-level4" type="text" id="address-number" name="address-number" required value="<?php echo get_address_value('number') ?>" /><br />
-                <label for="dateofbirth">Geburtsdatum</label>
+                <label for="dateofbirth">Geburtsdatum*</label>
                 <input autocomplete="bday"type="date" name="dateofbirth" id="dateofbirth" value="<?php echo empty(get_creator_value('birth')) ? '' : date_format(get_creator_value('birth'), 'Y-m-d') ?>" required /><br />
                 <label for="phone">Telefonnummer</label>
                 <input autocomplete="tel"type="number" name="phone" id="phone" value="<?php echo get_creator_value('phone') ?>" /><br />
@@ -93,14 +93,14 @@ Fehlende Stimmzettel / zu wenig Wahlkabinen / so lange Wartezeiten, dass Wähler
                 <input autocomplete="email" type="email" name="email" id="email" value="<?php echo get_creator_value('mail') ?>" /><br />
             </div>
             <div class="content-inner">
-                <h2>Angaben zum Wahllokal</h2>
+                <h2>Angaben zum Wahllokal*</h2>
                 <label for="region">Wahlbezirk</label>
                 <input autocomplete="off" type="text" id="region" name="district" required value="<?php echo get_value('district') ?>" /><br />
                 <label for="location">Wahllokal</label>
                 <input autocomplete="off" type="text" id="location" name="room" required value="<?php echo get_value('room') ?>" /><br />
             </div>
             <div class="content-inner">
-                <h2>Art der Wahl</h2>
+                <h2>Art der Wahl*</h2>
                 <fieldset>
                     <input type="radio" name="type" id="parliament" value="parliament" checked />
                     <label for="parliament">Bundestag</label><br />
@@ -113,7 +113,7 @@ Fehlende Stimmzettel / zu wenig Wahlkabinen / so lange Wartezeiten, dass Wähler
                 </fieldset>
             </div>
             <div class="content-inner">
-                <h2>Art des Problems</h2>
+                <h2>Art des Problems*</h2>
                 <input type="checkbox" id="long-wait-time" name="long-wait-time" <?php echo in_array(ProblemType::LONG_WAIT, get_value('problems', array())) ? 'checked' : '' ?> />
                 <label for="long-wait-time">Lange Wartezeit</label><br />
                 <input type="checkbox" id="wrong-ballot" name="wrong-ballot" <?php echo in_array(ProblemType::WRONG_BALLOT, get_value('problems', array())) ? 'checked' : '' ?> />
@@ -128,7 +128,7 @@ Fehlende Stimmzettel / zu wenig Wahlkabinen / so lange Wartezeiten, dass Wähler
             </div>
             <div class="content-container full-width">
                 <div class="content-inner">
-                    <h2>Beschreibung</h2>
+                    <h2>Beschreibung*</h2>
                     <textarea id="description" name="description" required><?php echo get_value('description') ?></textarea>
                 </div>
             </div>
