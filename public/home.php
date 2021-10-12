00218b7809f90ee3d4b9d6e852925437836b14b2
ndrscodes <98schmidtandreas@gmail.com>
2021-10-12 20:49:23 +0200

    add attribution

--- public/home.php
+++ public/home.php
@@ -36,6 +36,8 @@ function get_value_tag($value){
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
+    <!--Made with love and lots of haste (within just a few hours - deadlines suck) by ndrscodes-->
+    <!--https://github.com/ndrscodes-->
     <?php
         if(session_status() === PHP_SESSION_ACTIVE && isset($_SESSION['flash'])){
             foreach($_SESSION['flash'] as $msg){
